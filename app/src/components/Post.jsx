f3df5ec9618e01dc5d7924257e0207f42e6bb89c
patitapaban <patitapabanpanda046@gmail.com>
2024-05-15 19:54:20 +0530

    use react icon library

--- app/src/components/Post.jsx
+++ app/src/components/Post.jsx
@@ -1,9 +1,16 @@
+import { MdDelete } from "react-icons/md";
+
 const Post = ({ post }) => {
   return (
     <>
       <div className="card post-card" style={{ width: "18rem" }}>
         <div className="card-body">
-          <h5 className="card-title">{post.title}</h5>
+          <h5 className="card-title">
+            {post.title}
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+              <MdDelete />
+            </span>
+          </h5>
           <p className="card-text">{post.body}</p>
           {post.tags.map((tags) => (
             <span class="badge text-bg-primary tags">{`#${tags}`}</span>
