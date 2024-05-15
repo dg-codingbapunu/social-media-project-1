798c4bf6bb3692589ce9a9b57ea0446eb0a107bd
patitapaban <patitapabanpanda046@gmail.com>
2024-05-15 13:28:09 +0530

    create Sidebar

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,11 +3,14 @@ import "./App.css";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <>
+      <Sidebar></Sidebar>
       <Header></Header>
+
       <Footer></Footer>
     </>
   );
