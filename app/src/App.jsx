fb04276c00dba4923ea8e751c8f3becc7e760f8d
patitapaban <patitapabanpanda046@gmail.com>
2024-05-15 13:07:59 +0530

    create footer

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import "./App.css";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Header></Header>
+      <Footer></Footer>
     </>
   );
 };
