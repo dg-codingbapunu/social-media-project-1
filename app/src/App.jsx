e5391ca251f84c499581f7ee8e492630261a8344
patitapaban <patitapabanpanda046@gmail.com>
2024-05-15 12:59:34 +0530

    create header

---
+++ app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+};
+
+export default App;
